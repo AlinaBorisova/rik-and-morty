620b0179a0c97bbfeb0d347852e454fce4af1119
Алина Борисова <b-alina1999.borisova@yandex.ru>
2026-02-15 20:04:22 +0100

    feat: Создана главная траница с приветствием

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RootLayout } from "./routes/RootLayout/RootLayout";
 import { ErrorPage } from "./routes/ErrorPage/ErrorPage";
+import { HomePage } from "./routes/HomePage/HomePage";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }
+    ]
   }
 ]);
