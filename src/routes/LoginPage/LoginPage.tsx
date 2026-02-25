34643007d84fa80d9461acbdc7f7a649d9ef921c
Алина Борисова <b-alina1999.borisova@yandex.ru>
2026-02-25 21:52:17 +0100

    feat: Добавлена базовая логика на страницу LoginPage

--- src/routes/LoginPage/LoginPage.tsx
+++ src/routes/LoginPage/LoginPage.tsx
@@ -1,7 +1,15 @@
+import { Signin, type SigninForm } from "../../components/SignIn/SignIn";
+
 export function LoginPage() {
+
+  const handleEnter = (inputs: SigninForm) => {
+    console.log("Отправлено", inputs);
+    alert(`Welcome, ${inputs.email}`)
+  }
+  
   return (
     <>
-    <h1>Login</h1>
+      <Signin onSubmit={handleEnter}/>
     </>
   )
 }
