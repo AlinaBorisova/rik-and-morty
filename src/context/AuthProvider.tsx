e5709dd4b8270a57d8f87db84023ddb90e090371
Алина Борисова <b-alina1999.borisova@yandex.ru>
2026-02-25 21:54:08 +0100

    feat: Создан AuthProvider, отвечает за контекст авторизации пользователя

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+interface AuthContextValue {
+  user: string | null;
+  signin: (newUser: string, callback: () => void) => void;
+  signout: (callback: () => void) => void;
+}
+
+const AuthContext = createContext<AuthContextValue | null>(null);
+
+export function useAuth(): AuthContextValue | null {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<string | null>(null);
+
+  const signin = (newUser: string, callback: () => void) => {
+    setUser(newUser);
+    callback();
+  }
+
+  const signout = (callback: () => void) => {
+    setUser(null);
+    callback();
+  }
+
+  const value = {
+    user,
+    signin,
+    signout
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
