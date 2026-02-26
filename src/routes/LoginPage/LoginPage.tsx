721bd96abab01f779613a7453cd4ca0c8588652f
Алина Борисова <b-alina1999.borisova@yandex.ru>
2026-02-26 20:39:16 +0100

    fix: Исправлена ошибка множественного ренедера при входе

--- src/routes/LoginPage/LoginPage.tsx
+++ src/routes/LoginPage/LoginPage.tsx
@@ -1,21 +1,35 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { Signin, type SigninForm } from "../../components/SignIn/SignIn";
 import { useAuth } from "../../context/AuthProvider";
+import { useEffect } from "react";
+
+interface LocationState {
+  from?: {
+    pathname: string;
+  };
+}
 
 export function LoginPage() {
   const navigate = useNavigate();
   const location = useLocation();
+  const state = location.state as LocationState;
   const auth = useAuth();
 
-  console.log(location);
-  const from = location.state?.from?.pathname || '/';
+  const from = state?.from?.pathname || '/';
+
+  useEffect(() => {
+    if (auth?.user) {
+      navigate(from, { replace: true });
+    }
+  }, [auth?.user, navigate, from]);
 
   const handleEnter = (inputs: SigninForm) => {
-    console.log("Отправлено", inputs);
     auth?.signin(inputs.email, () => {
       navigate(from, { replace: true });
     });
-  }
+  };
+
+  if (auth?.user) return null;
   
   return (
     <>
