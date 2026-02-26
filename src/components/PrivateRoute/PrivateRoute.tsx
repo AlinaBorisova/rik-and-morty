94c3afd708f611809b60945d397a2e3e354bb5e6
Алина Борисова <b-alina1999.borisova@yandex.ru>
2026-02-26 20:00:55 +0100

    feat: Добавлена логика перенаправления пользователя после авторизации с учетом его предыдущей страницы

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,12 +1,12 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider";
-
 
 export function PrivateRoute({ children }: { children: React.ReactNode }) {
   const auth = useAuth();
+  const location = useLocation();
 
   if (!auth || auth.user === null) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
   return children;
--- src/routes/LoginPage/LoginPage.tsx
+++ src/routes/LoginPage/LoginPage.tsx
@@ -1,10 +1,20 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import { Signin, type SigninForm } from "../../components/SignIn/SignIn";
+import { useAuth } from "../../context/AuthProvider";
 
 export function LoginPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
+
+  console.log(location);
+  const from = location.state?.from?.pathname || '/';
 
   const handleEnter = (inputs: SigninForm) => {
     console.log("Отправлено", inputs);
-    alert(`Welcome, ${inputs.email}`)
+    auth?.signin(inputs.email, () => {
+      navigate(from, { replace: true });
+    });
   }
   
   return (
