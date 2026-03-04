18f01e4da2f570b321d7d2e3b2ad27dd2688aeb3
Алина Борисова <b-alina1999.borisova@yandex.ru>
2026-03-04 20:48:30 +0100

    feat: Изменены типы для получения данных и добавлена обработка ошибок

--- src/utils/loaders.ts
+++ src/utils/loaders.ts
@@ -1,22 +1,35 @@
-export const loadJsonArray = async <T>(path: string): Promise<T[]> => {
+export const loadJsonArray = async <T>(path: string): Promise<T> => {
   try {
     const response = await fetch(path);
 
-    return (await response.json()) as T[];
+    if (!response.ok) {
+      throw new Response(await response.text(), {
+        status: response.status,
+        statusText: response.statusText,
+      });
+    }
+
+    const data = await response.json();
+
+    return data as T;
   } catch (error) {
+    if (error instanceof Response) throw error;
     console.log(error);
     throw new Response('Not Found', { status: 404 });
   }
 };
 
-export const loadJsonItem = async<T extends { id: number }> (path: string, id: string): Promise<T> => {
+export const loadJsonItem = async<T extends { id: number }>(path: string, id: string): Promise<T> => {
   const propsId = Number(id);
   if (Number.isNaN(propsId)) throw new Response('Not Found', { status: 404 });
 
-  const data = await loadJsonArray<T>(path);
+  const data = await loadJsonArray<{ results: T[] }>(path);
 
-    const findItem = data.find((item) => item.id === propsId);
-    if (!findItem) throw new Response('Not Found', { status: 404 });
+  const listItems = Array.isArray(data) ? data : data.results;
 
-    return findItem;
+  const findItem = listItems.find((item: { id: number }) => item.id === propsId);
+
+  if (!findItem) throw new Response('Not Found', { status: 404 });
+
+  return findItem as T;
 };
